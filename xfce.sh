cb11a549c0e44f7aff5d86c2592d166cff241e3d
sdhz151 <yanghui140@gmail.com>
2024-04-10 15:53:02 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo dnf update
-sudo dnf install dejavu-fonts liberation-fonts gnu-*-fonts google-*-fonts
-sudo dnf install xorg-*
-sudo dnf install xfwm4 xfdesktop xfce4-* xfce4-*-plugin network-manager-applet *fonts
-sudo dnf install lightdm lightdm-gtk
+dnf update
+dnf install dejavu-fonts liberation-fonts gnu-*-fonts google-*-fonts
+dnf install xorg-*
+dnf install xfwm4 xfdesktop xfce4-* xfce4-*-plugin network-manager-applet *fonts
+dnf install lightdm lightdm-gtk
 echo 'user-session=xfce' >> /etc/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf
-sudo systemctl start lightdm
-sudo systemctl enable lightdm
-sudo systemctl set-default graphical.target
+systemctl start lightdm
+systemctl enable lightdm
+systemctl set-default graphical.target
 systemctl disable gdm
 
